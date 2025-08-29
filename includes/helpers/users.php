939334868f6c4227f75acf477c7ab35092e8f3d7
Scott Weaver <scottmweaver@gmail.com>
2025-08-28 23:53:33 -0700

    Refactor get_user_role_label function for consistent formatting and strict comparison

--- includes/helpers/users.php
+++ includes/helpers/users.php
@@ -16,11 +16,11 @@ namespace WP_Pulse\Helpers\Users;
  * @return string The label for the role.
  */
 function get_user_role_label( string $role ): string {
-    global $wp_roles;
+	global $wp_roles;
 
-    if (true === isset( $wp_roles->role_names[ $role ] ) ) {
-        return $wp_roles->role_names[ $role ];
-    }
+	if ( true === isset( $wp_roles->role_names[ $role ] ) ) {
+		return $wp_roles->role_names[ $role ];
+	}
 
-    return $role;
+	return $role;
 }
