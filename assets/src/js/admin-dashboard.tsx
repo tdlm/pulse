9a92f854049660fa6f120046d962bbf4f1632a5a
Scott Weaver <scottmweaver@gmail.com>
2025-09-03 19:04:55 -0700

    Refactor admin settings script for improved readability; enable eslint for admin dashboard file.

--- assets/src/js/admin-dashboard.tsx
+++ assets/src/js/admin-dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
--- assets/src/js/admin-settings.ts
+++ assets/src/js/admin-settings.ts
@@ -3,32 +3,32 @@ import apiFetch from '@wordpress/api-fetch';
 import '../scss/admin-settings.scss';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-
-    const checkboxKeepDays: HTMLInputElement | null = document.querySelector( '.pulse-keep-days' );
-    const checkboxKeepForever: HTMLInputElement | null = document.querySelector( '.pulse-keep-forever' );
-
-    if (checkboxKeepDays && checkboxKeepForever) {
-        // On load, if the keep forever checkbox is checked, hide the keep days checkbox.
-        if (checkboxKeepForever?.checked) {
-            checkboxKeepDays?.closest('tr')?.classList.add( 'hidden' );
-        }
-
-        // On change, if the keep forever checkbox is checked, hide the keep days checkbox.
-        // If the keep forever checkbox is unchecked, show the keep days checkbox.
-        checkboxKeepForever?.addEventListener( 'change', ( e: Event ) => {
-            e.preventDefault();
-
-            if ((e.target as HTMLInputElement)?.checked) {
-                checkboxKeepDays?.closest('tr')?.classList.add( 'hidden' );
-            } else {
-                checkboxKeepDays?.closest('tr')?.classList.remove( 'hidden' );
-            }
-        } );
-    }
-
-	const resetDatabaseButton = document.querySelector(
-		'#reset_all_pulses'
+	const checkboxKeepDays: HTMLInputElement | null =
+		document.querySelector( '.pulse-keep-days' );
+	const checkboxKeepForever: HTMLInputElement | null = document.querySelector(
+		'.pulse-keep-forever'
 	);
+
+	if ( checkboxKeepDays && checkboxKeepForever ) {
+		// On load, if the keep forever checkbox is checked, hide the keep days checkbox.
+		if ( checkboxKeepForever?.checked ) {
+			checkboxKeepDays?.closest( 'tr' )?.classList.add( 'hidden' );
+		}
+
+		// On change, if the keep forever checkbox is checked, hide the keep days checkbox.
+		// If the keep forever checkbox is unchecked, show the keep days checkbox.
+		checkboxKeepForever?.addEventListener( 'change', ( e: Event ) => {
+			e.preventDefault();
+
+			if ( ( e.target as HTMLInputElement )?.checked ) {
+				checkboxKeepDays?.closest( 'tr' )?.classList.add( 'hidden' );
+			} else {
+				checkboxKeepDays?.closest( 'tr' )?.classList.remove( 'hidden' );
+			}
+		} );
+	}
+
+	const resetDatabaseButton = document.querySelector( '#reset_all_pulses' );
 
 	if ( resetDatabaseButton ) {
 		resetDatabaseButton?.addEventListener( 'click', ( e: Event ) => {
