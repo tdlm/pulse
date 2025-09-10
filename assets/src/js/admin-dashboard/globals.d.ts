b4cbc99cb294fd919c104997725de1d6e2f245e1
Scott Weaver <scottmweaver@gmail.com>
2025-09-09 19:27:15 -0700

    Add global type definitions for PulseAdminDashboard and PulseAdminPulseDetail in admin dashboard.

---
+++ assets/src/js/admin-dashboard/globals.d.ts
@@ -0,0 +1,23 @@
+import { Records, Record } from "./types";
+
+declare global {
+  interface Window {
+    PulseAdminDashboard: Records & {
+      settings: {
+        admin_url: string;
+        dashboard_base_url: string;  
+        live_updates_enabled: boolean;
+      };
+    };
+    PulseAdminPulseDetail: {
+      meta: Array<{
+        key: string;
+        value: string;
+      }>;
+      record: Record;
+    };
+  }
+}
+
+// This export statement is crucial for module augmentation
+export {};
